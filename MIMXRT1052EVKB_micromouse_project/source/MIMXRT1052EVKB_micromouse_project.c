0fa0c22081ccbb6c49b295ac05782a7d38328b64
QuarterShotofEspresso <ratnodeepb@gmail.com>
2019-12-19 05:29:06 -0800

    Great FAT Big Gigantic modification to model code

--- MIMXRT1052EVKB_micromouse_project/source/MIMXRT1052EVKB_micromouse_project.c
+++ MIMXRT1052EVKB_micromouse_project/source/MIMXRT1052EVKB_micromouse_project.c
@@ -61,10 +61,10 @@ int main(void) {
     volatile static int i = 0 ;
     /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
-        i++ ;
+       i++ ;
         /* 'Dummy' NOP to allow source level single stepping of
             tight while() loop */
-        __asm volatile ("nop");
+       __asm volatile ("nop");
     }
     return 0 ;
 }
--- mm_model_code_IMXRT1052/mm_model_code_IMXRT1052.ino
+++ mm_model_code_IMXRT1052/mm_model_code_IMXRT1052.ino
@@ -8,78 +8,120 @@
 #define KD_MR_DATA_INPUT_PIN A6
 
 // IR reciver pairs inputs
-#define LEFT_IR_PIN A7
-#define MIDD_IR_PIN A8
-#define RIGH_IR_PIN A9
+#define LEFT_IR_PIN   A7
+#define MIDDLE_IR_PIN A8
+#define RIGHT_IR_PIN  A9
 
-#define LEFT_IR_EXPEC
-#define MIDD_IR_EXPEC
-#define RIGH_IR_EXPEC
-
+#define RM_GND_PIN  
 #define RM_PWM_PIN  A10
+#define LM_GND_PIN  
 #define LM_PWM_PIN  A11
 
+// TEST:: if test runs are sucessful, try concatenating both default PWMs into one
+#define RIGHT_MOTOR_DEFAULT_PWM 150
+#define LEFT_MOTOR_DEFAULT_PWM  150
+
+#define LED_CONTROL 13
+
 /* Additional constants used across multiple functions */
-int rightMotorPWM = 0, leftMotorPWM = 0;
-double kp_l, ki_l, kd_l, kp_r, ki_r, kd_r;
+double kpLeft, kiLeft, kdLeft, kpRight, kiRight, kdRight;
 int error, errorHead, t;
 
  /*
   *  brief: function stores values between 0 to 1 as floating point values 
   *  because all constants are recorded as values from a pot-aided voltage divider.
   */
-void updatekPID (void) {
-  
-  kp_l = analogRead(KP_ML_DATA_INPUT_PIN) / 1023.0;
-  ki_l = analogRead(KI_ML_DATA_INPUT_PIN) / 1023.0;
-  kd_l = analogRead(KD_ML_DATA_INPUT_PIN) / 1023.0;
-  
-  kp_r = analogRead(KP_MR_DATA_INPUT_PIN) / 1023.0;
-  ki_r = analogRead(KI_MR_DATA_INPUT_PIN) / 1023.0;
-  kd_r = analogRead(KD_MR_DATA_INPUT_PIN) / 1023.0;
+void InitializekPID (void) {
+
+  // constants, k, for the left motor PID
+  kpLeft =  analogRead(KP_ML_DATA_INPUT_PIN) / 1023.0;
+  kiLeft =  analogRead(KI_ML_DATA_INPUT_PIN) / 1023.0;
+  kdLeft =  analogRead(KD_ML_DATA_INPUT_PIN) / 1023.0;
+
+  // constants, k, for the right motor PID
+  kpRight = analogRead(KP_MR_DATA_INPUT_PIN) / 1023.0;
+  kiRight = analogRead(KI_MR_DATA_INPUT_PIN) / 1023.0;
+  kdRight = analogRead(KD_MR_DATA_INPUT_PIN) / 1023.0;
 
   return;
 }
 
 /*
  * brief: determines the adjustment required to correct the PWM of both motors
+ *        and adds the modification to both the left and right default PWMs.
  */
-void pidWrite (void) {
+void computePIDandUpdate (void) {
   
   static int errorPrev = 0;       // stores previous error measurement. Initialized to 0 for first measurement
   static int tPrev = 0;           // stores previous error measurement. Initialized to 0 for first measurement
   static int errorSum = error;    // stores previous error measurement. Initialized to error for first measurement
+  static int rightMotorPWMadjustment = 0;
+  static int leftMotorPWMadjustment = 0;
   
-  rightMotorPWM += (kp_l * error) + (ki_l * (errorSum * (t - tPrev))) + (kd_l * (error - errorPrev)/(t - tPrev));
-  leftMotorPWM  += (kp_r * error) + (ki_r * (errorSum * (t - tPrev))) * (kd_r * (error - errorPrev)/(t - tPrev));
+  rightMotorPWMadjustment = (kpLeft * error) + (kiLeft * (errorSum * (t - tPrev))) + (kdLeft * (error - errorPrev)/(t - tPrev));
+  leftMotorPWMadjustment  = (kpRight * error) + (kiRight * (errorSum * (t - tPrev))) + (kdRight * (error - errorPrev)/(t - tPrev));
+  
+  // NOTE:: A feautre of the Integral term in PID is that it maintains memory of what happened in the past.
+  // meaning any manufacturing offset within the motor that is left unaccounted for is actually accounted for by the integreal term
+  analogWrite(RM_PWM_PIN, RIGHT_MOTOR_DEFAULT_PWM + rightMotorPWMadjustment);
+  analogWrite(LM_PWM_PIN, LEFT_MOTOR_DEFAULT_PWM - leftMotorPWMadjustment);
+  
+  errorPrev = error;  // store the prior error into errorPrev for following D of PID
+  tPrev = t;          // store t into tPrev for following PID computation
+  t = millis();       // update t
+  errorSum += error;  // add current error errorSum for following I of PID
 
-  analogWrite(RM_PWM_PIN, rightMotorPWM);
-  analogWrite(LM_PWM_PIN, leftMotorPWM);
+  return;
+}
+
+
+/*
+ * FUNCITON: COMPUTE ERROR
+ * @brief: Logs the ideal IR sensor values from the start of the maze.
+ *         Error is calculated 
+ */
+void computeError (void) {
+
+  // record ExpectedDelta;
+  static int ExpectedDelta = analogRead(LEFT_IR_PIN) - analogRead(RIGHT_IR_PIN);
   
-  errorPrev = error;
-  tPrev = t;
-  errorSum += error;
+  // compute the error. NOTE::: consider mapping values to tweak/stablize error adjustment rate
+  error = ExpectedDelta - (analogRead(LEFT_IR_PIN) - analogRead(RIGHT_IR_PIN));
+  
+  return;
+}
+
+/*
+ * FUNCTION: INVERT LEFT MOTOR DIRECTION
+ * @brief: Flips the assigned left motor directions when called
+ */
+void InvertLEFTmotorDirection(void) {
+
+  digitalWrite(LM_PWM_PIN, LOW);
+  analogWrite(LM_GND_PIN, LEFT_MOTOR_DEFAULT_PWM);
 
   return;
 }
 
 /*
- * brief: takes inputs from IR pains and converts to calculable values
+ * FUNCTION: REVERT LEFT MOTOR DIRECTION
+ * @brief: Unflips the flipped left motor directions
  */
+void RevertLEFTmotorDirection(void) {
+  
+  digitalWrite(LM_GND_PIN, LOW);
+  analogWrite(LM_PWM_PIN, LEFT_MOTOR_DEFAULT_PWM);
 
- /// Need to adjust Error function it's barely effective with direction
-void determineError (void) {
-  error = pow(analogRead(LEFT_IR_PIN) - LEFT_IR_EXPEC, 2) + pow(analogRead(RIGH_IR_PIN) - RIGH_IR_EXPEC, 2);
-  errorHead = analogRead(MIDD_IR_PIN) - MIDD_IR_EXPEC;  // error function for head
   return;
 }
 
-// set threshold error before mouse strikes the wall
-void determineError (void) {
-  if (errorHead_threshold <= errorHead) {
-    analogWrite(RM_PWM_PIN, rightMotorPWM);
-    analogWrite(LM_PWM_PIN, leftMotorPWM);
-  }
+void flashLED(int onTime) {
+  
+  digitalWrite(LED_CONTROL, HIGH);
+  delay(onTime);
+  digitalWrite(LED_CONTROL, LOW);
+
+  return;
 }
 
 void setup (void) {
@@ -87,13 +129,32 @@ void setup (void) {
    *  INITIALIZE ALL GPIO porst to respective purposes
    *  
    */
-   
-  updatekPID();
+
+  // initialize all constants, k, for PID computations
+  /* 
+   * NOTE:: Initialze PWMs should ONLY be called once at the beggining. 
+   * This is because the PWM can only be 
+   */
+  InitializekPID();
+  
+  // instuct user loop and ExpectedDelta is to be recorded
+  flashLED(2000);
+  
+  return;
 }
 
 
 void loop (void) {
-  determineError();
-  pidWrite();
-  updatekPID();
+  
+  computeError();
+  computePIDandUpdate();
+  
+  // while the HEAD IR pair is a particular distance from the wall. Invert the motor's pins
+  if (analogRead(MIDDLE_IR_PIN) > HEAD_THRESHOLD) {
+    InvertLEFTmotorDirection();
+    digitalWrite(LED_CONTROL, HIGH);
+  } else {
+    RevertLEFTmotorDirection();
+    digitalWrite(LED_CONTROL, LOW);
+  }
 }
